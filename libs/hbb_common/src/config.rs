320b0c919259f3c7dbb75ba6c858a8818af27e76
LIDS0 <a2001216a@gmail.com>
2025-04-12 09:28:34 +0800

    config.rs

--- libs/hbb_common/src/config.rs
+++ libs/hbb_common/src/config.rs
@@ -100,7 +100,7 @@ const CHARS: &[char] = &[
     'm', 'n', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z',
 ];
 
-pub const RENDEZVOUS_SERVERS: &[&str] = &["lids.cf"];
+pub const RENDEZVOUS_SERVERS: &[&str] = &["lids.x3322.net"];
 pub const PUBLIC_RS_PUB_KEY: &str = "";
 
 pub const RS_PUB_KEY: &str = match option_env!("RS_PUB_KEY") {
